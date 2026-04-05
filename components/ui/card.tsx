bde58fde1225d05e1b4e57dd48b85fdff7f769ac
joacojachow <joacojachow@hotmail.com>
2026-04-05 18:34:09 -0300

    clean

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -83,9 +83,7 @@ function CardFooter({ className, ...props }: React.ComponentProps<'div'>) {
 export {
   Card,
   CardHeader,
-  CardFooter,
   CardTitle,
-  CardAction,
   CardDescription,
   CardContent,
 }
